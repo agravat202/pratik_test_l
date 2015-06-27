a5888fad23d8dbdb7c8f1e7fc2d1debc479b8c32
Pratik Agravat <agravat202@gmail.com>
2015-06-27 11:33:31 +0530

    Revert "learning controller n rouths"
    This reverts commit 89960e43d20246b57e3d07b16d08d0f65d45642c.

--- resources/views/header.blade.php
+++
@@ -1,9 +0,0 @@
-<htm>
-    <head>
-        <title>This is hi</title>
-    </head>
-    <body>
-        <div class="contender" style="height: 200px; background-color: #269abc;">
-        @yield('content')
-        </div>
-        
