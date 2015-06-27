3c89ef87e7c32d731f2857fc8d68325c84238c11
Pratik Agravat <agravat202@gmail.com>
2015-06-27 12:06:34 +0530

    Create header.blace.php

---
+++ resources/views/header.blace.php
@@ -0,0 +1,9 @@
+<htm>
+    <head>
+        <title>This is hi</title>
+    </head>
+    <body>
+        <div class="contender" style="height: 200px; background-color: #269abc;">
+        @yield('content')
+        </div>
+        
